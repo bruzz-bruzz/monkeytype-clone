85f636b2d27085c4eb123b0018338be5b95f479e
kfr <rahh28308@gmail.com>
2026-05-28 21:45:34 +0800

    a

--- monkeytype/src/components/App.tsx
+++ monkeytype/src/components/App.tsx
@@ -205,7 +205,7 @@ export default function App() {
   <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z"/>
 </svg></button>
       <h2 className='text-xl text-center'>{remainingTime}s remaining. {start === false? 'Type to start!':''}</h2>
-      <h1 className='text-xl'>{promptTxt.split(' ').length < count && validatePrompt(haveNumbers,havePunctuation) === false? 'Generating...' : promptTxt}</h1>
+      <h1 className='text-xl'>{promptTxt.split(' ').length < count || validatePrompt(haveNumbers,havePunctuation) === false ? 'Generating...' : promptTxt}</h1>
     </div>
     <div className={`p-6 ${textColor}`}>
       <textarea value={userInput} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>{ 
